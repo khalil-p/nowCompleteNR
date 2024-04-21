2b9aae1ec0afaa63d093d0b4d0ce9317037a8ada
khalil-p <pkhalilmk@gmail.com>
2024-04-21 09:30:21 +0530

    complete nr

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,33 @@ import { IoHelpBuoyOutline } from "react-icons/io5";
 import { FaSignInAlt } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
 import { Input } from "antd";
+import { MdStars } from "react-icons/md";
+import resList from "./demoData.json";
 const Header = () => {
   return (
     <>
       <nav className="flex items-center w-full justify-between mb-10 proxima-semi-bold text-base">
-        <div className="w-10">
+        <div className="w-8">
           <img src={swiggyLogo} alt="logo" />
         </div>
         <div className="container text-right">
-          <ul className="flex justify-end gap-x-20 items-center">
-            <li className="flex items-center gap-x-2.5">
+          <ul className="flex justify-end gap-x-20 items-center ">
+            <li className="flex items-center gap-x-2.5 cursor-pointer">
               <div className="w-80">
                 <Input placeholder="Basic usage" />
               </div>
               <FaSearch /> Search
             </li>
-            <li className="flex items-center gap-x-2.5">
+            <li className="flex items-center gap-x-2.5 cursor-pointer">
               <BiSolidOffer /> Offers
             </li>
-            <li className="flex items-center gap-x-2.5">
+            <li className="flex items-center gap-x-2.5 cursor-pointer">
               <IoHelpBuoyOutline /> Help
             </li>
-            <li className="flex items-center gap-x-2.5">
+            <li className="flex items-center gap-x-2.5 cursor-pointer">
               <FaSignInAlt /> Sign In
             </li>
-            <li className="flex items-center gap-x-2.5">
+            <li className="flex items-center gap-x-2.5 cursor-pointer">
               <FaCartShopping /> Cart
             </li>
           </ul>
@@ -39,15 +41,43 @@ const Header = () => {
     </>
   );
 };
-const ResCard = () => {
+// linear-gradient(rgba(27, 30, 36, 0) 0%, rgb(27, 30, 36) 84.21%)
+const ResCard = ({ resData }) => {
   return (
     <>
-      <div>
-        <div>
+      <div className="w-[calc(100%-920px)] hover:scale-90 cursor-pointer transition-all">
+        <div className="relative rounded-2xl overflow-hidden">
           <img
-            src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597"
+            src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${resData.info.cloudinaryImageId}`}
             alt="res_img"
+            className=" object-cover w-[100%] h-[calc(150px)]"
           />
+          <div
+            className="absolute bottom-0 left-0 right-0 text-white pl-2 "
+            style={{
+              background:
+                "linear-gradient(rgba(27, 30, 36, 0) 0%, rgb(27, 30, 36) 84.21%)",
+            }}
+          >
+            <h3 className="proxima-cond-black font-extrabold text-[22px] ">
+              ITEMS AT ₹179
+            </h3>
+          </div>
+        </div>
+        <div className="resInfo">
+          <div className="basis-grotesque-pro-medium text-lg">
+            {resData.info.name}
+          </div>
+          <div className="basis-grotesque-pro-medium text-base flex items-center gap-1">
+            <MdStars size={20} color="green" /> {resData.info.avgRating} •{" "}
+            {resData.info.sla.slaString}
+          </div>
+          <div className="cusines basis-grotesque-pro-regular text-base color-cl">
+            {resData.info.cuisines.join(", ")}
+          </div>
+          <div className="basis-grotesque-pro-regular text-base color-cl">
+            {resData.info.areaName}
+          </div>
         </div>
       </div>
     </>
@@ -56,10 +86,12 @@ const ResCard = () => {
 function App() {
   return (
     <>
-      <div className="py-10 px-20">
+      <div className="py-5 px-20">
         <Header />
-        <div>
-        <ResCard/>
+        <div className="flex flex-wrap justify-between gap-y-7">
+          {resList.map((i) => {
+            return <ResCard resData={i} />;
+          })}
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
+
+const plugin = require('tailwindcss/plugin')
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        h1: { fontSize: theme("fontSize.2xl") },
+        h2: { fontSize: theme("fontSize.xl") },
+        h3: { fontSize: theme("fontSize.lg"),fontWeight:theme("fontWeight.bold") },
+      });
+    }),
+  ],
+};
